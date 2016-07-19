f4a9b78da9d000ca0f48f52692674dd2b20d386f
AlexDaniels <alexdanielsonline@gmail.com>
2016-07-19 01:41:25 -0700

    Added icons to board page buttons and fixed positioning bugs, will need to continue to test on other devices and browser versions, particularly the presentation machine

--- public/javascripts/board/Board.js
+++ public/javascripts/board/Board.js
@@ -27,19 +27,20 @@ var Board = React.createClass({
 				<div className='row'>
 					<div className='col-md-1 col-xs-12'>
 						<div style={this.state.toolStyle} className="input-group" role="group" aria-label="Basic example">
-							<div className='col-xs-3 col-md-12'>
-								<input id='slider' type="range" min='0' max='360'></input>
+							<div id='slider' className='col-xs-3 col-md-12'>
+								<input type="range" min='0' max='360'></input>
 								<div>Angle</div>
 							</div>
 							
 							<div id='noteColor' className="col-xs-1 col-md-12 button-group colorPicker" role="group">
 								<button id="btnGroupDrop1" type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>	
 								<div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
 
 								</div>
 								<div>Note</div>
@@ -47,20 +48,21 @@ var Board = React.createClass({
 							<div id='noteColor' className="col-xs-1 col-md-12 button-group colorPicker" role="group">
 								<button id="btnGroupDrop2" type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>	
 								<div className="dropdown-menu" aria-labelledby="btnGroupDrop2">
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
-									<button className="col-md-12 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
+									<button className="col-md-2 col-xs-2 dropdown-item btn"></button>
 
 								</div>
 								<div>Text</div>
 							</div>
-							<button id='save' type="button" className="btn btn-secondary col-xs-1 col-xs-offset-1 col-md-offset-0 col-md-12"></button>
-							<button id='change' type="button" className="btn btn-secondary col-xs-1 col-md-12"></button>
-							<button id='delete' type="button" className="btn btn-secondary col-xs-1 col-md-12"></button>
-							<button id='cancel' type="button" className="btn btn-secondary col-xs-1 col-md-12"></button>
-							<button id='chat' type="button" className="btn btn-secondary col-xs-1 col-xs-offset-1 col-md-offset-0 col-md-12"></button>
+							<button id='change' type="button" className="btn glyphicon glyphicon-chevron-right btn-secondary col-md-offset-0 col-xs-offset-1 col-xs-1 col-md-12"></button>
+							<button id='save' type="button" className="btn glyphicon glyphicon-ok btn-secondary col-xs-1 col-md-12"></button>
+							<button id='delete' type="button" className="btn glyphicon glyphicon-trash btn-secondary col-xs-1 col-md-12"></button>
+							<button id='cancel' type="button" className="btn glyphicon glyphicon-remove btn-secondary col-xs-1 col-md-12"><span class="glyphicons glyphicons-remove-sign"></span></button>
+							<button id='chat' type="button" className="btn glyphicon glyphicon-comment btn-secondary col-xs-1 col-md-12"></button>
 						</div>
 					</div>
 					<div className='col-lg-11 col-md-10 col-md-offset-1 col-lg-offset-0 col-xs-12' style={this.state.boardStyle}></div>
